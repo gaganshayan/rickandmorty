a08fd883d09d726f43821e5354c4f37159c4791b
Shayan Gagan <shayangagan@Shayans-MacBook-Pro.local>
2020-10-08 23:15:22 -0500

    Linked video and styled it

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -4,15 +4,12 @@ import "./About.css";
 function About() {
     return(
         <div className="About">
-            <h1>About</h1>
-            <br />
-            <h3>This application allows fans of the adult comedy series 'Rick and Morty' to generate random characters from throughout the infinite parallel universes' and displays an image of their characters, some statistics including their name, gender and life status.</h3>
+            <p>About</p>
+            <h1>This application allows fans of the adult comedy series 'Rick and Morty' to generate random characters from throughout the infinite parallel universes' and displays an image of their characters, some statistics including their name, gender and life status.</h1>
             <hr />
-            <br />
-            <h3>I used the <a href="https://rickandmortyapi.com/">Rick and Morty API</a> to display the characters and statistics from the series.</h3>
+            <h1>I used the <a href="https://rickandmortyapi.com/">Rick and Morty API</a> to display the characters and statistics from the series.</h1>
             <hr />
-            <br />
-            <h3>Thanks for checking out my app. If you have any comments or questions, drop me a <a href="mailto:gaganshayan@gmail.com">line</a>.</h3>
+            <h1>Thanks for checking out my app. If you have any comments or questions, drop me a <a href="mailto:gaganshayan@gmail.com">line</a>.</h1>
             <br />
         </div>
     )
--- src/Components/Video/Video.js
+++ src/Components/Video/Video.js
@@ -4,10 +4,10 @@ import "./Video.css";
 function Video () {
     return (
     <div className="Video">
-        <h1>LET'S GET SCHWIFTY!</h1>
+        <p>LET'S GET SCHWIFTY!</p>
         <hr />
             <div className="youtube">
-                <iframe width="560" height="315" src="https://www.youtube.com/embed/4ctK1aoWuqY?controls=0" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                <iframe width="760" height="515" src="https://www.youtube.com/embed/4ctK1aoWuqY?controls=0" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
             </div>
     </div>
     )
